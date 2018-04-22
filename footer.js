36b45b167ff37f7706b77e3983c418108ad224bb
Gurkirt "Garry" Chahal <gurkirtchahal@garrysc.com>
2018-04-22 14:23:38 -0700

    Added index page, product page, and contact page

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.write('\
+	<hr />\
+\
+	<footer id="footer">\
+		<p>Copyright &copy; 2018 Odin, Garry Chahal\
+	</footer>\
+');
---
+++ navbar.js
@@ -0,0 +1,17 @@
+document.write('\
+	<div id="header">\
+		<div class="container">\
+			<h1>Odin</h1>\
+		</div>\
+	</div>\
+\
+	<nav id="navbar">\
+		<div class="container">\
+			<ul>\
+				<a href="index.html"><li>Home</li></a>\
+				<a href="products.html"><li>Products</li></a>\
+				<a href="contact.html"><li>Contact Us</li></a>\
+			</ul>\
+		</div>\
+	</nav>\
+');
